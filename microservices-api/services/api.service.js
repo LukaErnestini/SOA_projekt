ce49af96d4a0fd3bed3b07712f1557db183f0606
LukaErnestini <luka.ernestini@gmail.com>
2022-01-14 15:04:52 +0000

    user register, login, auth working

--- microservices-api/services/api.service.js
+++ microservices-api/services/api.service.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const ApiGateway = require("moleculer-web");
+const _ = require("lodash");
 
 /**
  * @typedef {import('moleculer').Context} Context Moleculer's Context
@@ -27,9 +28,7 @@ module.exports = {
 			{
 				path: "/api",
 
-				whitelist: [
-					"**"
-				],
+				whitelist: ["**"],
 
 				// Route-level Express middlewares. More info: https://moleculer.services/docs/0.14/moleculer-web.html#Middlewares
 				use: [],
@@ -38,7 +37,7 @@ module.exports = {
 				mergeParams: true,
 
 				// Enable authentication. Implement the logic into `authenticate` method. More info: https://moleculer.services/docs/0.14/moleculer-web.html#Authentication
-				authentication: false,
+				authentication: true,
 
 				// Enable authorization. Implement the logic into `authorize` method. More info: https://moleculer.services/docs/0.14/moleculer-web.html#Authorization
 				authorization: false,
@@ -47,9 +46,7 @@ module.exports = {
 				// The gateway will dynamically build the full routes from service schema.
 				autoAliases: true,
 
-				aliases: {
-
-				},
+				aliases: {},
 
 				/** 
 				 * Before call hook. You can check the request.
@@ -82,20 +79,20 @@ module.exports = {
 				bodyParsers: {
 					json: {
 						strict: false,
-						limit: "1MB"
+						limit: "1MB",
 					},
 					urlencoded: {
 						extended: true,
-						limit: "1MB"
-					}
+						limit: "1MB",
+					},
 				},
 
 				// Mapping policy setting. More info: https://moleculer.services/docs/0.14/moleculer-web.html#Mapping-policy
 				mappingPolicy: "all", // Available values: "all", "restrict"
 
 				// Enable/disable logging
-				logging: true
-			}
+				logging: true,
+			},
 		],
 
 		// Do not log client side errors (does not log an error response when the error.code is 400<=X<500)
@@ -105,18 +102,16 @@ module.exports = {
 		// Logging the response data. Set to any log level to enable it. E.g. "info"
 		logResponseData: null,
 
-
 		// Serve assets from "public" folder. More info: https://moleculer.services/docs/0.14/moleculer-web.html#Serve-static-files
 		assets: {
 			folder: "public",
 
 			// Options to `server-static` module
-			options: {}
-		}
+			options: {},
+		},
 	},
 
 	methods: {
-
 		/**
 		 * Authenticate the request. It check the `Authorization` token value in the request header.
 		 * Check the token value & resolve the user by the token.
@@ -131,26 +126,32 @@ module.exports = {
 		 */
 		async authenticate(ctx, route, req) {
 			// Read the token from header
-			const auth = req.headers["authorization"];
-
-			if (auth && auth.startsWith("Bearer")) {
-				const token = auth.slice(7);
-
-				// Check the token. Tip: call a service which verify the token. E.g. `accounts.resolveToken`
-				if (token == "123456") {
-					// Returns the resolved user. It will be set to the `ctx.meta.user`
-					return { id: 1, name: "John Doe" };
-
-				} else {
-					// Invalid token
-					throw new ApiGateway.Errors.UnAuthorizedError(ApiGateway.Errors.ERR_INVALID_TOKEN);
-				}
-
-			} else {
-				// No token. Throw an error or do nothing if anonymous access is allowed.
-				// throw new E.UnAuthorizedError(E.ERR_NO_TOKEN);
-				return null;
+			let token;
+			if (req.headers.authorization) {
+				let type = req.headers.authorization.split(" ")[0];
+				if (type === "Token" || type === "Bearer")
+					token = req.headers.authorization.split(" ")[1];
 			}
+
+			let user;
+			if (token) {
+				// Verify JWT token
+				try {
+					user = await ctx.call("users.resolveToken", { token });
+					if (user) {
+						this.logger.info("Authenticated via JWT: ", user.email);
+						// Reduce user fields (it will be transferred to other nodes)
+						ctx.meta.user = _.pick(user, ["_id", "email", "image"]);
+						ctx.meta.token = token;
+						ctx.meta.userID = user._id;
+					}
+				} catch (err) {
+					// Ignored because we continue processing if user doesn't exists
+				}
+			}
+
+			if (req.$action.auth == "required" && !user)
+				throw new ApiGateway.Errors.UnAuthorizedError();
 		},
 
 		/**
@@ -171,7 +172,6 @@ module.exports = {
 			if (req.$action.auth == "required" && !user) {
 				throw new ApiGateway.Errors.UnAuthorizedError("NO_RIGHTS");
 			}
-		}
-
-	}
+		},
+	},
 };
---
+++ microservices-api/services/users.service.js
@@ -0,0 +1,247 @@
+const { MoleculerClientError } = require("moleculer").Errors;
+
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const DbService = require("../mixins/db.mixin");
+//const CacheCleanerMixin = require("../mixins/cache.cleaner.mixin");
+
+module.exports = {
+	name: "users",
+	mixins: [DbService("users")],
+
+	/**
+	 * Default settings
+	 */
+	settings: {
+		/** Secret for JWT */
+		JWT_SECRET: process.env.JWT_SECRET || "jwt-secret",
+
+		/** Public fields */
+		fields: ["_id", "firstname", "lastname", "email", "bio", "image"],
+
+		/** Validator schema for entity */
+		entityValidator: {
+			firstname: { type: "string" },
+			lastname: { type: "string" },
+			email: { type: "email" },
+			password: { type: "string", min: 6 },
+			bio: { type: "string", optional: true },
+			image: { type: "string", optional: true },
+		},
+	},
+
+	/**
+	 * Actions
+	 */
+	actions: {
+		/**
+		 * Register a new user
+		 *
+		 * @actions
+		 * @param {Object} user - User entity
+		 *
+		 * @returns {Object} Created entity & token
+		 */
+		create: {
+			params: {
+				user: { type: "object" },
+			},
+			async handler(ctx) {
+				let entity = ctx.params.user;
+				await this.validateEntity(entity);
+				if (entity.email) {
+					const found = await this.adapter.findOne({
+						email: entity.email,
+					});
+					if (found)
+						return Promise.reject(
+							new MoleculerClientError(
+								"Email exists!",
+								422,
+								"Email exists!",
+								[{ field: "email", message: "Email exists" }]
+							)
+						);
+				}
+				entity.password = bcrypt.hashSync(entity.password, 10);
+				entity.bio = entity.bio || "";
+				entity.image = entity.image || null;
+				entity.createdAt = new Date();
+
+				const doc = await this.adapter.insert(entity);
+				const user = await this.transformDocuments(ctx, {}, doc);
+				return this.entityChanged("created", user, ctx).then(
+					() => user
+				);
+			},
+		},
+
+		/**
+		 * Login with username & password
+		 *
+		 * @actions
+		 * @param {Object} user - User credentials
+		 *
+		 * @returns {Object} Logged in user with token
+		 */
+		login: {
+			rest: "POST /users/login",
+			params: {
+				user: {
+					type: "object",
+					props: {
+						email: { type: "email" },
+						password: { type: "string", min: 1 },
+					},
+				},
+			},
+			async handler(ctx) {
+				const { email, password } = ctx.params.user;
+
+				const user = await this.adapter.findOne({ email });
+				if (!user)
+					throw new MoleculerClientError(
+						"Email or password is invalid!",
+						422,
+						"",
+						[{ field: "email", message: "is not found" }]
+					);
+
+				const res = await bcrypt.compare(password, user.password);
+				if (!res)
+					throw new MoleculerClientError("Wrong password!", 422, "", [
+						{ field: "email", message: "is not found" },
+					]);
+
+				// Transform user entity (remove password and all protected fields)
+				const doc = await this.transformDocuments(ctx, {}, user);
+				return await this.transformEntity(doc, true, ctx.meta.token);
+			},
+		},
+
+		/**
+		 * Get user by JWT token (for API GW authentication)
+		 *
+		 * @actions
+		 * @param {String} token - JWT token
+		 *
+		 * @returns {Object} Resolved user
+		 */
+		resolveToken: {
+			cache: {
+				keys: ["token"],
+				ttl: 60 * 60, // 1 hour
+			},
+			params: {
+				token: "string",
+			},
+			async handler(ctx) {
+				const decoded = await new this.Promise((resolve, reject) => {
+					jwt.verify(
+						ctx.params.token,
+						this.settings.JWT_SECRET,
+						(err, decoded) => {
+							if (err) return reject(err);
+
+							resolve(decoded);
+						}
+					);
+				});
+
+				if (decoded.id) return this.getById(decoded.id);
+			},
+		},
+
+		/**
+		 * Get current user entity.
+		 * Auth is required!
+		 *
+		 * @actions
+		 *
+		 * @returns {Object} User entity
+		 */
+		me: {
+			auth: "required",
+			rest: "GET /user",
+			cache: {
+				keys: ["#userID"],
+			},
+			async handler(ctx) {
+				const user = await this.getById(ctx.meta.userID);
+				if (!user)
+					throw new MoleculerClientError("User not found!", 400);
+
+				const doc = await this.transformDocuments(ctx, {}, user);
+				return await this.transformEntity(doc, true, ctx.meta.token);
+			},
+		},
+	},
+
+	/**
+	 * Methods
+	 */
+	methods: {
+		/**
+		 * Generate a JWT token from user entity
+		 *
+		 * @param {Object} user
+		 */
+		generateJWT(user) {
+			const today = new Date();
+			const exp = new Date(today);
+			exp.setDate(today.getDate() + 60);
+
+			return jwt.sign(
+				{
+					id: user._id,
+					username: user.username,
+					exp: Math.floor(exp.getTime() / 1000),
+				},
+				this.settings.JWT_SECRET
+			);
+		},
+
+		/**
+		 * Transform returned user entity. Generate JWT token if neccessary.
+		 *
+		 * @param {Object} user
+		 * @param {Boolean} withToken
+		 */
+		transformEntity(user, withToken, token) {
+			if (user) {
+				//user.image = user.image || "https://www.gravatar.com/avatar/" + crypto.createHash("md5").update(user.email).digest("hex") + "?d=robohash";
+				user.image = user.image || "";
+				if (withToken) user.token = token || this.generateJWT(user);
+			}
+
+			return { user };
+		},
+
+		/**
+		 * Transform returned user entity as profile.
+		 *
+		 * @param {Context} ctx
+		 * @param {Object} user
+		 * @param {Object?} loggedInUser
+		 */
+		async transformProfile(ctx, user, loggedInUser) {
+			//user.image = user.image || "https://www.gravatar.com/avatar/" + crypto.createHash("md5").update(user.email).digest("hex") + "?d=robohash";
+			user.image =
+				user.image ||
+				"https://static.productionready.io/images/smiley-cyrus.jpg";
+
+			if (loggedInUser) {
+				const res = await ctx.call("follows.has", {
+					user: loggedInUser._id.toString(),
+					follow: user._id.toString(),
+				});
+				user.following = res;
+			} else {
+				user.following = false;
+			}
+
+			return { profile: user };
+		},
+	},
+};
