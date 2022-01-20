e58613ba0a3969a69b9619c76a9dac261d930737
LukaErnestini <luka.ernestini@gmail.com>
2022-01-20 07:59:04 +0000

    Can create admin account using secret

--- microservices-api/services/boat.service.js
+++ microservices-api/services/boat.service.js
@@ -150,7 +150,7 @@ module.exports = {
 		},
 
 		/**
-		 * List all boats
+		 * List all boats of user
 		 * Auth is required!
 		 * @actions
 		 *
@@ -199,6 +199,41 @@ module.exports = {
 			async handler(ctx) {
 				const res = await this.adapter.removeById(ctx.params.id);
 				await this.entityChanged("removed", res, ctx);
+			},
+		},
+
+		/**
+		 * List all boats
+		 * Auth is required!
+		 * Admin only
+		 * @actions
+		 *
+		 */
+		boatsOfUser: {
+			auth: "required",
+			rest: "GET /mine",
+			async handler(ctx) {
+				// TODO ERROR: find() query not working !! only with findOne it works, otherwise ignored
+				const doc = await this.adapter.find({
+					ownerID: ctx.meta.userID,
+				});
+				this.logger.info("doc: ", doc);
+				this.logger.info("doc: ", ctx.meta.userID);
+
+				// get rid of boats without owners
+				let filtered = doc.filter((el) => {
+					return el.ownerID !== null;
+				});
+
+				filtered = filtered.filter((el) => {
+					return (
+						el.ownerID.toString().trim() ==
+						ctx.meta.userID.toString().trim()
+					);
+				});
+
+				const json = await this.transformDocuments(ctx, {}, filtered);
+				return json;
 			},
 		},
 	},
